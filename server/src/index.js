be01323cca43f051db1443a02f6f868accdaabbd
d.jayalath@stylesoftware.co <d.jayalath@stylesoftware.co>
2018-11-09 15:55:25 +0530

    adding server

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => { 
+    res.send('welcome')
+})
+
+app.listen(3000, err => {
+    if(err){
+        console.error(err);
+    }else{
+        console.log('Server listen on port 3000');
+    }
+})
